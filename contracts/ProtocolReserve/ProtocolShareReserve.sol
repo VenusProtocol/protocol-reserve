28de15b390c958ef529f6a10dd7640d58b7795db
Narayan Prusty <arsenal.narayan@gmail.com>
2023-05-16 12:54:13 +0100

    feat: destination addresses and distribution percent

--- contracts/ProtocolReserve/ProtocolShareReserve.sol
+++ contracts/ProtocolReserve/ProtocolShareReserve.sol
@@ -1,22 +1,56 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.13;
 
-import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import { SafeERC20Upgradeable, IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
-import "../Utils/ExponentialNoError.sol";
-import "../Interfaces/IRiskFund.sol";
-import "../Helpers/ReserveHelpers.sol";
-import "../Interfaces/IProtocolShareReserve.sol";
+import { ExponentialNoError } from "../Utils/ExponentialNoError.sol";
+import { IRiskFund } from "../Interfaces/IRiskFund.sol";
+import { ReserveHelpers } from "../Helpers/ReserveHelpers.sol";
+import { IProtocolShareReserve } from "../Interfaces/IProtocolShareReserve.sol";
 
 contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, ReserveHelpers, IProtocolShareReserve {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    address private protocolIncome;
-    address private riskFund;
-    // Percentage of funds not sent to the RiskFund contract when the funds are released, following the project Tokenomics
-    uint256 private constant protocolSharePercentage = 70;
-    uint256 private constant baseUnit = 100;
+    /// @notice it represents the type of vToken income
+    enum IncomeType {
+        SPREAD,
+        LIQUIDATION,
+        UNDEFINED
+    }
+
+    /// @notice address of RiskFundSwapper contract
+    address public RISK_FUND_SWAPPER;
+
+    /// @notice address of XVSVaultSwapper contract
+    address public XVS_VAULT_SWAPPER;
+
+    /// @notice address of Prime contract
+    address public PRIME;
+
+    /// @notice address of DAO destination
+    address public DAO;
+
+    /// @notice income distribution percentage to risk fund for income from non-prime market
+    uint256 public constant SCHEMA_TWO_MARKET_RISK_FIND_PERCENT = 48;
+
+    /// @notice income distribution percentage to risk fund for income from market spread in prime market
+    uint256 public constant SCHEMA_ONE_RISK_FIND_PERCENT = 20;
+
+    /// @notice income distribution percentage to xvs vault for income from non-prime market
+    uint256 public constant SCHEMA_TWO_XVS_VAULT_PERCENT = 26;
+
+    /// @notice income distribution percentage to xvs vault for income from market spread in prime market
+    uint256 public constant SCHEMA_ONE_XVS_VAULT_PERCENT = 20;
+
+    /// @notice income distribution percentage to DAO for income from non-prime market
+    uint256 public constant SCHEMA_TWO_DAO_VAULT_PERCENT = 26;
+
+    /// @notice income distribution percentage to DAO for income from market spread in prime market
+    uint256 public constant SCHEMA_ONE_DAO_VAULT_PERCENT = 26;
+
+    /// @notice income distribution percentage to prime for income from market spread in prime market
+    uint256 public constant PRIME_PERCENT = 20;
 
     /// @notice Emitted when funds are released
     event FundsReleased(address comptroller, address asset, uint256 amount);
@@ -33,17 +67,28 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, Re
 
     /**
      * @dev Initializes the deployer to owner.
-     * @param _protocolIncome The address protocol income will be sent to
-     * @param _riskFund Risk fund address
+     * @param riskFundSwapper The address of RiskFundSwapper contract
+     * @param xvsVaultSwapper The address of XVSVaultSwapper contract
+     * @param dao The address to send DAO funds to
+     * @param prime The address of Prime contract
      */
-    function initialize(address _protocolIncome, address _riskFund) external initializer {
-        require(_protocolIncome != address(0), "ProtocolShareReserve: Protocol Income address invalid");
-        require(_riskFund != address(0), "ProtocolShareReserve: Risk Fund address invalid");
+    function initialize(
+        address riskFundSwapper, 
+        address xvsVaultSwapper,
+        address dao,
+        address prime
+    ) external initializer {
+        require(riskFundSwapper != address(0), "ProtocolShareReserve: RiskFundSwapper address invalid");
+        require(xvsVaultSwapper != address(0), "ProtocolShareReserve: XVSVaultSwapper address invalid");
+        require(dao != address(0), "ProtocolShareReserve: DAO address invalid");
+        require(prime != address(0), "ProtocolShareReserve: Prime address invalid");
 
         __Ownable2Step_init();
 
-        protocolIncome = _protocolIncome;
-        riskFund = _riskFund;
+        RISK_FUND_SWAPPER = riskFundSwapper;
+        XVS_VAULT_SWAPPER = xvsVaultSwapper;
+        DAO = dao;
+        PRIME = prime;
     }
 
     /**
@@ -63,27 +108,27 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, Re
      * @param amount Amount to release
      * @return Number of total released tokens
      */
-    function releaseFunds(address comptroller, address asset, uint256 amount) external returns (uint256) {
-        require(asset != address(0), "ProtocolShareReserve: Asset address invalid");
-        require(amount <= poolsAssetsReserves[comptroller][asset], "ProtocolShareReserve: Insufficient pool balance");
+    // function releaseFunds(address comptroller, address asset, uint256 amount) external returns (uint256) {
+    //     require(asset != address(0), "ProtocolShareReserve: Asset address invalid");
+    //     require(amount <= poolsAssetsReserves[comptroller][asset], "ProtocolShareReserve: Insufficient pool balance");
 
-        assetsReserves[asset] -= amount;
-        poolsAssetsReserves[comptroller][asset] -= amount;
-        uint256 protocolIncomeAmount = mul_(
-            Exp({ mantissa: amount }),
-            div_(Exp({ mantissa: protocolSharePercentage * expScale }), baseUnit)
-        ).mantissa;
+    //     assetsReserves[asset] -= amount;
+    //     poolsAssetsReserves[comptroller][asset] -= amount;
+    //     uint256 protocolIncomeAmount = mul_(
+    //         Exp({ mantissa: amount }),
+    //         div_(Exp({ mantissa: protocolSharePercentage * expScale }), baseUnit)
+    //     ).mantissa;
 
-        IERC20Upgradeable(asset).safeTransfer(protocolIncome, protocolIncomeAmount);
-        IERC20Upgradeable(asset).safeTransfer(riskFund, amount - protocolIncomeAmount);
+    //     IERC20Upgradeable(asset).safeTransfer(protocolIncome, protocolIncomeAmount);
+    //     IERC20Upgradeable(asset).safeTransfer(riskFund, amount - protocolIncomeAmount);
 
-        // Update the pool asset's state in the risk fund for the above transfer.
-        IRiskFund(riskFund).updateAssetsState(comptroller, asset);
+    //     // Update the pool asset's state in the risk fund for the above transfer.
+    //     IRiskFund(riskFund).updateAssetsState(comptroller, asset);
 
-        emit FundsReleased(comptroller, asset, amount);
+    //     emit FundsReleased(comptroller, asset, amount);
 
-        return amount;
-    }
+    //     return amount;
+    // }
 
     /**
      * @dev Update the reserve of the asset for the specific pool after transferring to the protocol share reserve.
