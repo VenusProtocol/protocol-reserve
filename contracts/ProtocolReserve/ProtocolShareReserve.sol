bc8f342ecf14294e7dbfbf13014db44707cd71aa
Narayan Prusty <arsenal.narayan@gmail.com>
2023-05-17 22:55:50 +0100

    feat: dynamic destinations and split funds based of comptroller

--- contracts/ProtocolReserve/ProtocolShareReserve.sol
+++ contracts/ProtocolReserve/ProtocolShareReserve.sol
@@ -19,17 +19,14 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, IP
         TWO
     }
 
-    /// @notice address of RiskFundSwapper contract
-    address public RISK_FUND_SWAPPER;
-
-    /// @notice address of XVSVaultSwapper contract
-    address public XVS_VAULT_SWAPPER;
+    struct DistributionConfig {
+        Schema schema;
+        uint256 percentage;
+        address destination;
+    }
 
     /// @notice address of Prime contract
     address public PRIME;
-
-    /// @notice address of DAO destination
-    address public DAO;
 
     /// @notice address of pool registry contract
     address public POOL_REGISTRY;
@@ -37,28 +34,10 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, IP
     /// @notice address of core pool comptroller contract
     address public CORE_POOL_COMPTROLLER;
 
-    /// @notice income distribution percentage to risk fund for income from non-prime market
-    uint256 public constant SCHEMA_TWO_MARKET_RISK_FIND_PERCENT = 48;
+    /// @notice comptroller => asset => schema => balance
+    mapping (address => mapping (address => mapping ( Schema => uint256 ))) public assetsReserves;
 
-    /// @notice income distribution percentage to risk fund for income from market spread in prime market
-    uint256 public constant SCHEMA_ONE_RISK_FIND_PERCENT = 20;
-
-    /// @notice income distribution percentage to xvs vault for income from non-prime market
-    uint256 public constant SCHEMA_TWO_XVS_VAULT_PERCENT = 26;
-
-    /// @notice income distribution percentage to xvs vault for income from market spread in prime market
-    uint256 public constant SCHEMA_ONE_XVS_VAULT_PERCENT = 20;
-
-    /// @notice income distribution percentage to DAO for income from non-prime market
-    uint256 public constant SCHEMA_TWO_DAO_VAULT_PERCENT = 26;
-
-    /// @notice income distribution percentage to DAO for income from market spread in prime market
-    uint256 public constant SCHEMA_ONE_DAO_VAULT_PERCENT = 26;
-
-    /// @notice income distribution percentage to prime for income from market spread in prime market
-    uint256 public constant PRIME_PERCENT = 20;
-
-    mapping (address => mapping ( Schema => uint256 )) public assetsReserves;
+   DistributionConfig[] public distributionTargets;
 
     /// @notice Emitted when funds are released
     event FundsReleased(address comptroller, address asset, uint256 amount);
@@ -66,9 +45,14 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, IP
     /// @notice Emitted when pool registry address is updated
     event PoolRegistryUpdated(address indexed oldPoolRegistry, address indexed newPoolRegistry);
 
-    // Event emitted after the updation of the assets reserves.
-    // amount -> reserve increased by amount.
-    event AssetsReservesUpdated(address indexed comptroller, address indexed asset, uint256 amount, IncomeType incomeType);
+    /// @notice Emitted when prime address is updated
+    event PrimeUpdated(address indexed oldPrime, address indexed newPrime);
+
+    /// @notice Event emitted after the updation of the assets reserves.
+    event AssetsReservesUpdated(address indexed comptroller, address indexed asset, uint256 amount, IncomeType incomeType, Schema schema);
+
+    /// @notice Event emitted after a income distribution target is configured
+    event DestinationConfigured(address indexed destination, uint percent, Schema schema);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -79,56 +63,80 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, IP
 
     /**
      * @dev Initializes the deployer to owner.
-     * @param riskFundSwapper The address of RiskFundSwapper contract
-     * @param xvsVaultSwapper The address of XVSVaultSwapper contract
-     * @param dao The address to send DAO funds to
-     * @param prime The address of Prime contract
+     * @param corePoolComptroller The address of core pool comptroller
      */
     function initialize(
-        address riskFundSwapper, 
-        address xvsVaultSwapper,
-        address dao,
-        address prime,
         address corePoolComptroller
     ) external initializer {
-        require(riskFundSwapper != address(0), "ProtocolShareReserve: RiskFundSwapper address invalid");
-        require(xvsVaultSwapper != address(0), "ProtocolShareReserve: XVSVaultSwapper address invalid");
-        require(dao != address(0), "ProtocolShareReserve: DAO address invalid");
-        require(prime != address(0), "ProtocolShareReserve: Prime address invalid");
-
+        require(corePoolComptroller != address(0), "ProtocolShareReserve: Core pool comptroller address invalid");
         __Ownable2Step_init();
-
-        RISK_FUND_SWAPPER = riskFundSwapper;
-        XVS_VAULT_SWAPPER = xvsVaultSwapper;
-        DAO = dao;
-        PRIME = prime;
         CORE_POOL_COMPTROLLER = corePoolComptroller;
     }
 
-     /**
+    /**
      * @dev Pool registry setter.
-     * @param _poolRegistry Address of the pool registry
+     * @param poolRegistry Address of the pool registry
      */
-    function setPoolRegistry(address _poolRegistry) external onlyOwner {
-        require(_poolRegistry != address(0), "ProtocolShareReserve: Pool registry address invalid");
+    function setPoolRegistry(address poolRegistry) external onlyOwner {
+        require(poolRegistry != address(0), "ProtocolShareReserve: Pool registry address invalid");
         address oldPoolRegistry = POOL_REGISTRY;
-        POOL_REGISTRY = _poolRegistry;
-        emit PoolRegistryUpdated(oldPoolRegistry, _poolRegistry);
+        POOL_REGISTRY = poolRegistry;
+        emit PoolRegistryUpdated(oldPoolRegistry, poolRegistry);
+    }
+
+    /**
+     * @dev Prime contract address setter.
+     * @param prime Address of the prime contract
+     */
+    function setPrime(address prime) external onlyOwner {
+        require(prime != address(0), "ProtocolShareReserve: Prime address invalid");
+        address oldPrime = PRIME;
+        PRIME = prime;
+        emit PrimeUpdated(oldPrime, prime);
+    }
+
+    /**
+     * @dev Add or update destination target based on destination address
+     * @param config configuration of the destination. 
+     */
+    function addOrUpdateDistributionConfig(DistributionConfig memory config) external onlyOwner {
+        uint256 total = 0;
+        bool updated = false;
+        for (uint i = 0; i < distributionTargets.length; i++) {
+            DistributionConfig storage _config = distributionTargets[i];
+            if (config.destination == _config.destination) {
+                total += config.percentage;
+
+                _config.destination = config.destination;
+                _config.percentage = config.percentage;
+                _config.schema = config.schema;
+                updated = true;
+            } else if (_config.schema == config.schema) {
+                total += _config.percentage;
+            }
+        }
+
+        if (updated == false) {
+            total += config.percentage;
+            distributionTargets.push(config);
+        }
+
+        require(total <= 100, "ProtocolShareReserve: total percent cannot be more than 100");
     }
 
     /**
      * @dev Release funds
      * @param assets assets to be released
     */
-    function releaseFunds(address[] memory assets) external {
+    function releaseFunds(address comptroller, address[] memory assets) external {
         for (uint i = 0; i < assets.length; i++) {
-            _releaseFund(assets[i]);
+            _releaseFund(comptroller, assets[i]);
         }
     }
 
-    function _releaseFund(address asset) internal {
-        uint256 schemaOneBalance = assetsReserves[asset][Schema.ONE];
-        uint256 schemaTwoBalance = assetsReserves[asset][Schema.ONE];
+    function _releaseFund(address comptroller, address asset) internal {
+        uint256 schemaOneBalance = assetsReserves[comptroller][asset][Schema.ONE];
+        uint256 schemaTwoBalance = assetsReserves[comptroller][asset][Schema.ONE];
 
         // Distribute schemaOneBalance based on SCHEMA_ONE_RISK_FIND_PERCENT, SCHEMA_ONE_XVS_VAULT_PERCENT, SCHEMA_ONE_DAO_VAULT_PERCENT and PRIME_PERCENT
 
@@ -161,7 +169,7 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, IP
         }
 
         uint256 currentBalance = IERC20Upgradeable(asset).balanceOf(address(this));
-        uint256 assetReserve = assetsReserves[asset][schema];
+        uint256 assetReserve = assetsReserves[comptroller][asset][schema];
 
         if (currentBalance > assetReserve) {
             uint256 balanceDifference;
@@ -169,8 +177,8 @@ contract ProtocolShareReserve is Ownable2StepUpgradeable, ExponentialNoError, IP
                 balanceDifference = currentBalance - assetReserve;
             }
             
-            assetsReserves[asset][schema] += balanceDifference;
-            emit AssetsReservesUpdated(comptroller, asset, balanceDifference, incomeType);
+            assetsReserves[comptroller][asset][schema] += balanceDifference;
+            emit AssetsReservesUpdated(comptroller, asset, balanceDifference, incomeType, schema);
         }
     }
 }
