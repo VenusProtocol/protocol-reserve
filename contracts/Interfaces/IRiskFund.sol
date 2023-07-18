69ac6a2ad46976a0ededa0b2607bec6d0c329219
Debugger022 <debugger022@gmail.com>
2023-07-18 13:56:46 +0530

    feat: risk fund contract

--- contracts/Interfaces/IRiskFund.sol
+++ contracts/Interfaces/IRiskFund.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.13;
 
+/**
+ * @title IRiskFund
+ * @author Venus
+ * @notice Interface implemented by `RiskFund`.
+ */
 interface IRiskFund {
-    function updateAssetsState(address comptroller, address asset) external;
+    function transferReserveForAuction(address comptroller, uint256 amount) external returns (uint256);
+
+    function updatePoolState(address comptroller, uint256 amount) external;
+
+    function poolReserves(address comptroller) external view returns (uint256);
 }
