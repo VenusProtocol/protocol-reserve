f18c2a08732192822fbd75177e63daf88164d90c
Debugger022 <debugger022@gmail.com>
2023-07-19 19:26:24 +0530

    refactor: ven-1748 risk fund V2

---
+++ contracts/Interfaces/IRiskFund.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.13;
+
+/**
+ * @title IRiskFund
+ * @author Venus
+ * @notice Interface implemented by `RiskFund`.
+ */
+interface IRiskFund {
+    function transferReserveForAuction(address comptroller, uint256 amount) external returns (uint256);
+
+    function updatePoolState(address comptroller, uint256 amount) external;
+}
