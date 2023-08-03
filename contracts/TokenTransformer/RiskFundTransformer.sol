e45db93885d9741d4af23625f549d5da0eba7905
Debugger022 <debugger022@gmail.com>
2023-08-03 15:39:10 +0530

    refactor: added reserve helpers methods

--- contracts/TokenTransformer/RiskFundTransformer.sol
+++ contracts/TokenTransformer/RiskFundTransformer.sol
@@ -6,17 +6,45 @@ import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC
 import { ResilientOracle } from "@venusprotocol/oracle/contracts/ResilientOracle.sol";
 
 import { AbstractTokenTransformer } from "./AbstractTokenTransformer.sol";
-import { ReserveHelpers } from "../Helpers/ReserveHelpers.sol";
 import { ensureNonzeroAddress } from "../Utils/Validators.sol";
 import { IPoolRegistry } from "../Interfaces/IPoolRegistry.sol";
+import { IComptroller } from "../Interfaces/IComptroller.sol";
 import { IRiskFund } from "../Interfaces/IRiskFund.sol";
 import { EXP_SCALE } from "../Utils/Constants.sol";
 
-contract RiskFundTransformer is AbstractTokenTransformer, ReserveHelpers {
+contract RiskFundTransformer is AbstractTokenTransformer {
     using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // Store the previous state for the asset transferred to ProtocolShareReserve combined(for all pools).
+    mapping(address => uint256) internal assetsReserves;
+
+    // Store the asset's reserve per pool in the ProtocolShareReserve.
+    // Comptroller(pool) -> Asset -> amount
+    mapping(address => mapping(address => uint256)) internal poolsAssetsReserves;
+
+    // Address of pool registry contract
+    address public poolRegistry;
+
+    /// @dev This empty reserved space is put in place to allow future versions to add new
+    /// variables without shifting down storage in the inheritance chain.
+    uint256[47] private __gap;
 
     /// @notice Emitted when pool registry address is updated
     event PoolRegistryUpdated(address indexed oldPoolRegistry, address indexed newPoolRegistry);
+
+    // Event emitted after the updation of the assets reserves.
+    // amount -> reserve increased by amount.
+    event AssetsReservesUpdated(address indexed comptroller, address indexed asset, uint256 amount);
+
+    /// @dev Get the Amount of the asset in the risk fund for the specific pool.
+    /// @param comptroller Comptroller address (pool)
+    /// @param asset Asset address.
+    /// @return Asset's reserve in risk fund.
+    function getPoolAssetReserve(address comptroller, address asset) external view returns (uint256) {
+        require(IComptroller(comptroller).isComptroller(), "ReserveHelpers: Comptroller address invalid");
+        require(asset != address(0), "ReserveHelpers: Asset address invalid");
+        return poolsAssetsReserves[comptroller][asset];
+    }
 
     /// @dev Pool registry setter
     /// @param poolRegistry_ Address of the pool registry
@@ -45,6 +73,32 @@ contract RiskFundTransformer is AbstractTokenTransformer, ReserveHelpers {
     ) public initializer {
         // Initialize AbstractTokenTransformer
         __AbstractTokenTransformer_init(accessControlManager_, priceOracle_, destinationAddress_);
+    }
+
+    /// @dev Update the reserve of the asset for the specific pool after transferring to risk fund
+    /// and transferring funds to the protocol share reserve
+    /// @param comptroller Comptroller address (pool)
+    /// @param asset Asset address.
+    function updateAssetsState(address comptroller, address asset) public {
+        require(IComptroller(comptroller).isComptroller(), "ReserveHelpers: Comptroller address invalid");
+        require(asset != address(0), "ReserveHelpers: Asset address invalid");
+        require(poolRegistry != address(0), "ReserveHelpers: Pool Registry address is not set");
+        require(
+            IPoolRegistry(poolRegistry).getVTokenForAsset(comptroller, asset) != address(0),
+            "ReserveHelpers: The pool doesn't support the asset"
+        );
+
+        uint256 currentBalance = IERC20Upgradeable(asset).balanceOf(address(this));
+        uint256 assetReserve = assetsReserves[asset];
+        if (currentBalance > assetReserve) {
+            uint256 balanceDifference;
+            unchecked {
+                balanceDifference = currentBalance - assetReserve;
+            }
+            assetsReserves[asset] += balanceDifference;
+            poolsAssetsReserves[comptroller][asset] += balanceDifference;
+            emit AssetsReservesUpdated(comptroller, asset, balanceDifference);
+        }
     }
 
     /// @notice Hook to perform after transforming tokens
