c0656d44f0500e23cde52705d5310fc8c8fd5c60
0xlucian <0xluciandev@gmail.com>
2023-12-13 13:02:14 +0200

    refactor: make deployment compatible with sepolia network

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,3 +1,5 @@
+import { Contracts as bnbMainnetAddresses } from "@venusprotocol/venus-protocol/networks/mainnet.json";
+import { Contracts as bnbTestnetAddresses } from "@venusprotocol/venus-protocol/networks/testnet.json";
 import BigNumber from "bignumber.js";
 
 BigNumber.config({
@@ -22,4 +24,17 @@ export const convertToBigInt = (amount: string | number, decimals: number) => {
 };
 
 export const ADDRESS_ONE = "0x0000000000000000000000000000000000000001";
-export const SEPOLIA_MULTISIG = "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb";
+
+interface ChainAddressesConfig {
+  [key: string]: string;
+}
+
+// Add all multisigs here (in case there is no NormalTimelock deployed on the chain we will fallback to this config)
+export const multisigs: ChainAddressesConfig = {
+  sepolia: "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb",
+};
+
+export const wBNBAddress: ChainAddressesConfig = {
+  bscmainnet: bnbMainnetAddresses.WBNB,
+  bsctestnet: bnbTestnetAddresses.WBNB,
+};
