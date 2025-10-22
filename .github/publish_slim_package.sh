c8a4cdc8e8c7493783214b3f4b59b575b8ba2404
Gleiser Oliveira <gleiser.dev@gmail.com>
2025-10-22 13:24:30 -0600

    feat: add slim package scripts

---
+++ .github/publish_slim_package.sh
@@ -0,0 +1,7 @@
+cd slim
+
+# Extract to PRE_RELEASE_TAG the tag in the version field of the package json, or the empty string if it doesn't exist
+PRE_RELEASE_TAG=$(jq -r '.version | if test("-") then capture("^[0-9]+\\.[0-9]+\\.[0-9]+(?<tag>-[a-zA-Z-]+)") | .tag else "" end' package.json)
+
+# Publish the package to a custom tag for slim versions
+npm publish --access public --tag slim$PRE_RELEASE_TAG
